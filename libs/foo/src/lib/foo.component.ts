4d831ec1a491e0188a1a88703c0b0e1e04cf2240
Emin Temiz <emin.vegan@gmail.com>
2024-10-04 10:11:30 +0200

    init

--- libs/foo/src/lib/foo.component.ts
+++ libs/foo/src/lib/foo.component.ts
@@ -1,16 +1,13 @@
-import {Component} from "@angular/core";
-import {BazComponent} from "@kreuzerk/release-it-baz";
+import { Component } from '@angular/core';
+import { BazComponent } from '@kreuzerk/release-it-baz';
 
 @Component({
   standalone: true,
   selector: 'release-it-foo',
   template: `
-      <h1>Improve foo</h1>
-      <release-it-baz/>
+    <h1></h1>
+    <release-it-baz />
   `,
-  imports: [
-    BazComponent
-  ]
+  imports: [BazComponent],
 })
-export class FooComponent {
-}
+export class FooComponent {}
